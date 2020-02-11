a2149b58151dddbbd3ee3bfdea4df08b1c7d9b8e
dima <tynindima@gmail.com>
2020-02-11 10:58:17 +0200

    Updates

---
+++ precache-manifest.1c2b84309c2f5c33c13a8ee79b853df9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ab8174f75a51a1ce02ad48a14964b9",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "81896d94f9cf39e7fc5b",
+    "url": "/cra-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1a9220d66250825c7423",
+    "url": "/cra-demo/static/js/2.d93e864c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra-demo/static/js/2.d93e864c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81896d94f9cf39e7fc5b",
+    "url": "/cra-demo/static/js/main.bdc48dd9.chunk.js"
+  },
+  {
+    "revision": "107a7a11fab40b66012c",
+    "url": "/cra-demo/static/js/runtime-main.b7e94f1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
