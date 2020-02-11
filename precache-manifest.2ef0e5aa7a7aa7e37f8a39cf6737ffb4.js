5e6fbdb18de97abe4efb3a618a93ead75e79e8b6
dima <tynindima@gmail.com>
2020-02-11 11:24:53 +0200

    Updates

---
+++ precache-manifest.2ef0e5aa7a7aa7e37f8a39cf6737ffb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a81ee3386e97f66027a4a6ef312876",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "671757cf77c5959b7cd4",
+    "url": "/cra-demo/static/js/2.b1939bae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra-demo/static/js/2.b1939bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d16832be30f42ca56c",
+    "url": "/cra-demo/static/js/main.d213309d.chunk.js"
+  },
+  {
+    "revision": "107a7a11fab40b66012c",
+    "url": "/cra-demo/static/js/runtime-main.b7e94f1e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cra-demo/precache-manifest.1c2b84309c2f5c33c13a8ee79b853df9.js"
+  "/cra-demo/precache-manifest.2ef0e5aa7a7aa7e37f8a39cf6737ffb4.js"
 );
 
 self.addEventListener('message', (event) => {
